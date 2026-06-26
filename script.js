e7b6a25a6bb2588fce16e4c1b7738be72a88f52f
Danish Suhail <134198476+Danish-Suhail@users.noreply.github.com>
2026-06-27 02:14:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 //your code here!
 
+const container = document.getElementById("infi-list");
+
+let itemCount = 0;
+
+function addItem(){
+	itemCount++;
+	const li = document.createElement("li");
+	li.textContent = `Item ${itemCount}`;
+	container.appendChild(li);
+}
+
+for(let i=0; i<10;i++){
+	addItem();
+}
+
+window.addEventListener('scroll', () => {
+	 const totalPageHeight = document.documentElement.offsetHeight;
+    // Current positions of user viewport bottom (rounded up)
+    const currentScrollPos = Math.ceil(window.innerHeight + window.scrollY);
+
+    // If we scroll to the bottom, add 2 more items
+    if (currentScrollPos >= totalPageHeight - 5) {
+        addItem();
+        addItem();
+    }
+});
+
